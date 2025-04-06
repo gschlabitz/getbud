9584d3ade18deba6876d893c0ef3121e4508d8a9
gschlabitz <guido.schlabitz@gmail.com>
2025-04-06 12:10:44 -0500

    Start with the SwiftUI app template.

---
+++ Getbud/Getbud/GetbudApp.swift
@@ -0,0 +1,17 @@
+//
+//  GetbudApp.swift
+//  Getbud
+//
+//  Created by Guido Schlabitz on 4/6/25.
+//
+
+import SwiftUI
+
+@main
+struct GetbudApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Getbud/GetbudTests/GetbudTests.swift
@@ -0,0 +1,17 @@
+//
+//  GetbudTests.swift
+//  GetbudTests
+//
+//  Created by Guido Schlabitz on 4/6/25.
+//
+
+import Testing
+@testable import Getbud
+
+struct GetbudTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
